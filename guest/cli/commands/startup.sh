8a08eea6cfb53c23e689206471590ae11d4c624c
Daryl Lozupone <daryl@newclarity.net>
2017-04-24 15:42:42 -0400

    Fix startup command wplib/wplib-box#201

--- guest/cli/commands/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-database="$(jq '.startup.database' < /vagrant/box.json)"
-webserver="$(jq '.startup.webserver' < /vagrant/box.json)"
-processvm="$(jq '.startup.processvm' < /vagrant/box.json)"
-
-docker start $database \
-  && docker start $webserver \
-  && docker start $processvm
