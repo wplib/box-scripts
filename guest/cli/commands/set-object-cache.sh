826aec0021f79c3bbff9aad01001c70d47f987fe
Daryl Lozupone <daryl@newclarity.net>
2017-04-26 12:08:18 -0400

    Add set-object-cache command

---
+++ guest/cli/commands/set-object-cache.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Switching object cache to $1"
+
+if [ ! "redis" == "$1" ] && [ ! "memcached" == "$1" ]; then
+    echo 'You must select either redis or memcached'
+    exit 1
+fi
+
+converse="memcached"
+if [ 'memcached' == $1 ]; then
+    converse='redis'
+fi
+
+docker stop $converse 2>&1 > /dev/null \
+  && docker start $1 2>&1 > /dev/null \
+  && sudo sudo sed -i "s/${converse}/$1/" /vagrant/box.json
